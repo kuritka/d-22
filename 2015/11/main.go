6ef2ca9ad35fc2906d7e214aeecedcfb34328a30
kuritka <kuritka@gmail.com>
2022-08-29 09:18:36 +0200

    wg

--- 2015/11/main.go
+++ 2015/11/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -23,26 +24,21 @@ type Order struct {
 	UnitPrice      float64
 }
 
-func extract() []*Order {
-	result := []*Order{}
+func extract(ch chan *Order) {
 	f, _ := os.Open("./2015/11/input/orders.txt")
 	defer f.Close()
 	r := csv.NewReader(f)
-	// one record  at time from a file and proceeding as non errors are encountered
-	// po jednom záznamu ze souboru a pokračování podle toho, jak se vyskytnou chyby.
-	// je to FOR cyklus, ktery bezi do konce souboru!!!
-	// something like  for i:=0 ; i< something; i++ {...}
 	for record, err := r.Read(); err == nil; record, err = r.Read() {
 		order := new(Order)
 		order.CustomerNumber, _ = strconv.Atoi(record[1])
 		order.PartNumber = record[0]
 		order.Quantity, _ = strconv.Atoi(record[2])
-		result = append(result, order)
+		ch <- order // 🟦 each record goes to channe;
 	}
-	return result
+	close(ch) // 🟦 we need to stop ange process in transform actor
 }
 
-func transform(orders []*Order) []*Order {
+func transform(inputCh, outputCh chan *Order) {
 	f, _ := os.Open("./2015/11/input/orders.txt")
 	//https://www.joeshaw.org/dont-defer-close-on-writable-files/
 	defer f.Close()
@@ -58,29 +54,47 @@ func transform(orders []*Order) []*Order {
 		productList[product.PartNumber] = product
 	}
 
-	for _, o := range orders {
-		time.Sleep(3 * time.Millisecond)
-		o.UnitPrice = productList[o.PartNumber].UnitPrice
-		o.UnitCost = productList[o.PartNumber].UnitCost
+	wg := sync.WaitGroup{}
+	for o := range inputCh { // 🟦
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, o *Order) {
+			time.Sleep(3 * time.Millisecond)
+			o.UnitPrice = productList[o.PartNumber].UnitPrice
+			o.UnitCost = productList[o.PartNumber].UnitCost
+			outputCh <- o // 🟦 isolated each actor
+			wg.Done()
+		}(&wg, o)
 	}
-	return orders
+	wg.Wait()
+	close(outputCh) // 🟦 close it to inform range in load actor
 }
 
-func load(orders []*Order) {
+func load(orders chan *Order, done chan bool) {
 	f, _ := os.Create("./2015/11/dest.txt")
 	defer f.Close()
 	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s\n", "Part Number", "Quantity", "Unit Cost", "Unit Price", "Total Cost", "Total Price")
-	for _, o := range orders {
-		time.Sleep(1 * time.Millisecond)
-		fmt.Fprintf(f, "%20s %15d %12.2f v%12.2f %15.2f %15.2f\n", o.PartNumber, o.Quantity, o.UnitCost, o.UnitPrice, o.UnitCost*float64(o.Quantity), o.UnitPrice*float64(o.Quantity))
+	wg := sync.WaitGroup{}
+	for o := range orders {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, o *Order) {
+			time.Sleep(1 * time.Millisecond)
+			fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n", o.PartNumber, o.Quantity, o.UnitCost, o.UnitPrice, o.UnitCost*float64(o.Quantity), o.UnitPrice*float64(o.Quantity))
+			wg.Done()
+		}(&wg, o)
 	}
+	wg.Wait()
+	done <- true
 }
 
 func main() {
 	start := time.Now()
-	runtime.GOMAXPROCS(1)
-	orders := extract()
-	orders = transform(orders)
-	load(orders)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	ch1 := make(chan *Order)
+	ch2 := make(chan *Order)
+	done := make(chan bool)
+	go extract(ch1)
+	go transform(ch1, ch2)
+	go load(ch2, done)
+	<-done
 	fmt.Printf("\nexecution time %s", time.Since(start))
 }
